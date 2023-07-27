a29bc7d52a6bd38dc7211b705e7b5c5e32d53b17
CadenArcher <cadenarcher45@gmail.com>
2023-07-27 15:07:12 -0400

    Should have done this earlier, but this was pretty streight forward. Made a letter fnction that check to see if what the user gave is a upper case or lower case. and if its in range or if its even a letter

--- Pick a letter, any letter/Pick a letter, any letter.cpp
+++ Pick a letter, any letter/Pick a letter, any letter.cpp
@@ -4,11 +4,54 @@
 #include <iomanip>
 #include<iostream>
 #include <fstream>
+#include <array>
 using namespace std;
 
 
 int main()
 {
   
+
+
 }
 
+
+void letter(char WhatLetter, int ChangeLetter) {
+
+	char LowerCase[27] = { "abcdefghijklmnopqrstuvwxyz" };
+	char UpperCase[27] = { "ABCDEFGHIJKLMNOPQRSTUVWXYZ" };
+	int LetterPlacement;
+
+
+	if (islower(WhatLetter) == true) {
+
+		for (int i = 0;i < 27; i++) {
+			if (WhatLetter == LowerCase[i]) {
+				LetterPlacement = i;
+			}
+		}
+		if (ChangeLetter + LetterPlacement < 26 && ChangeLetter + LetterPlacement > 0) {
+			cout << LowerCase[LetterPlacement + ChangeLetter];
+		}
+		else {
+			cout << "invalidRangeExpection";
+		}
+	}
+	else if (isupper(WhatLetter) == true) {
+		for (int i = 0;i < 27; i++) {
+			if (WhatLetter == UpperCase[i]) {
+				LetterPlacement = i;
+			}
+		}
+		if (ChangeLetter + LetterPlacement < 26 && ChangeLetter + LetterPlacement > 0) {
+			cout << UpperCase[LetterPlacement + ChangeLetter];
+		}
+		else {
+			cout << "invalidRangeExpection";
+		}
+	}
+	else {
+		cout << "Invaild Character Excpeption";
+	}
+
+}
